8b471d0af73099dc509e850447cf8466af73983c
B00084048 <b00084048@student.itb.ie>
2016-04-08 23:34:41 +0100

    added line

--- Login.php
+++ Login.php
@@ -1,6 +1,18 @@
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
     <title>Login</title>
-    
+    </head>
+<body>
+<form>
+<label class="register"><strong>Username:</strong></label>
+<input type="text" name="username" required class="register_input"/>
+
+<label class="register"><strong>Password</strong></label>
+<input type="password" name="password" required class="register_input"/>
+<button id="submit" type="submit" value="login">Login</button>
+</form>
+<p>Dont' have an account? <a href="Signup.php">Signup</a> Here</p>
+</body>
+
 </head>
 </html>
