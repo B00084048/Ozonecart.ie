8c522cd9b5b94845df9983d73f5ecd3e1bca0ed0
B00084048 <B00084048@student.itb.ie>
2016-04-08 22:59:05 +0100

    Added files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+
+
+<html>
+<head>
+<titel>Login</titel>
+ </head>
+<body?
+<label class="register"><strong>Username:</strong></label>
+<input type="text" name="username" required class="register_input" />
+
+ <label class="register"><strong>Password:</strong></label>
+ <input type="password" name="password" required class="register_input" />
+
+ <button id="submit" type="submit" value="login">Login</button>
+
+</form>
+<p>Don't have an account?<a href ="signup.php">Signup here</a></p>
+</body>
+
+</html>
+
