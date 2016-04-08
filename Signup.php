c4fb93bfd27cccf4275a168792debb4686966766
B00084048 <b00084048@student.itb.ie>
2016-04-08 23:43:50 +0100

    added line

---
+++ Signup.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>Sign up</title>
+    </head>
+<body>
+<form>
+    <label class="register"><strong>Username:</strong></label>
+    <input type="text" name="username" required class="register_input" />
+
+        <label class="register"><strong>Email:</strong></label>
+        <input type="text" required name="email" class="register_input" />
+
+            <label class="register"><strong>Password:</strong></label>
+        <input type="password" required  name="password"class="register_input" />
+
+
+        <button id="submit" type="submit" name="submit" value="signup">Sign up</button>
+
+    </form>
+    <p>Already have an account?<a href="login.php">Login here</a></p>
+
+</body>
+
+</html>
